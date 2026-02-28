69dfaf81b7781596eaab365990fb73ef530592d0
diegomaustem <diego.maustem@gmail.com>
2026-02-28 17:58:57 -0300

    :sparkles: feat: Create server main of project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import "reflect-metadata";
+import path from "path";
 import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { AuthorsResolver } from "./resolvers/authors-resolver";
 
 async function startServer() {
+    const schema = await buildSchema({
+        resolvers: [
+            AuthorsResolver,
+        ],
+        emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+    });
 
     const server = new ApolloServer({         
-    
+        schema,
     });
 
     const { url } = await   server.listen();
