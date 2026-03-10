17965889ea5a6bff8c92ec47eb25f7757fa9bd7e
diegomaustem <diego.maustem@gmail.com>
2026-03-10 20:28:04 -0300

    :sparkles: feat: Adjustment of resolver

--- src/resolvers/authors-resolver.ts
+++ src/resolvers/authors-resolver.ts
@@ -1,17 +1,27 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { CreateAuthorInput } from "../dtos/create-author-input";
 import { Author } from "../models/author-model";
+import { prisma } from "../db/prisma";
 
 @Resolver()
 export class AuthorsResolver {
 
     @Query(() => [Author])
     async getAllAuthors() {
-        return [new Author()];
+        const authors = await prisma.author.findMany();
+        return authors;
     } 
 
     @Mutation(() => Author)
     async createAuthor(@Arg("data") data: CreateAuthorInput) {
-        return new Author();
+        const newAuthor = await prisma.author.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                address: data.address,
+                age: data.age,      
+            },
+        });
+        return newAuthor;
     }   
 }
