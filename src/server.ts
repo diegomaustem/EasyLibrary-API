0464c3c1469a30233a78f823a1a59231801fb0a7
diegomaustem <diego.maustem@gmail.com>
2026-03-10 21:27:11 -0300

    :sparkles: feat: Adjustment init server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import "reflect-metadata";
+import dotenv from 'dotenv';
 import path from "path";
+
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { prisma } from "./db/prisma";
 import { AuthorsResolver } from "./resolvers/authors-resolver";
+
+dotenv.config();
 
 async function startServer() {
     const schema = await buildSchema({
         resolvers: [
             AuthorsResolver,
         ],
-        emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+        emitSchemaFile: path.resolve(process.cwd(), "schema.gql"),
     });
 
     const server = new ApolloServer({         
